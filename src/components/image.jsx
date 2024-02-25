05ca4703e4110a3ea79b75b6d196cdb2acc906fe
Ankit Kashyap <ankitkashyap6202347735@gmail.com>
2024-02-25 15:15:59 +0530

    Click events handled

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -1,5 +1,6 @@
 export default function Image({ link, redirect }) {
-  function handleClick() {
+  function handleClick(event) {
+    event.preventDefault();
     window.location.href = redirect;
   }
   return <img onClick={handleClick} src={link}></img>;
